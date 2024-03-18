936c6ead821e1b7ad124c06121dd711b742cd296
VeerasaiGadi <veerasaigadi9624@gmail.com>
2024-03-19 00:10:42 +0530

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SETTINGS: Map<string, string> = new Map([
+    ["CONVAI-API-KEY", "e6f6db881dd648f380cc51d6238297c8"],
+    ["CHARACTER-ID", "13b45924-e2c9-11ee-9ad5-42010a7be009"]
+]);;
